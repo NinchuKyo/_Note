5d15f2bc799dcc5456090d731a21734c59bf4df0
Duong Nguyen <davidnguyenT@gmail.com>
2014-02-04 05:32:32 -0600

    Modified server to produce login form

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ I added something...
 Testing git merge master
 
 Lyndon - Adding something and testing merge.
+
+David - Modified server to produce login form and result of login.  Note, it is
+currently a prototype.
--- server.py
+++ server.py
@@ -1,13 +1,18 @@
 from flask import Flask
+from flask import render_template
+from flask import request
 app = Flask(__name__)
 
 @app.route("/")
-def login():
-    return "Welcome to _Note</br>Please login with your Dropbox account."
+def login(name=None):
+    return render_template('test.html', name=name)
 
-@app.route("/home")
-def home():
-    return "You have logged in successfully.</br>Here's a list of your notes."
+@app.route("/result.html", methods=['GET', 'POST'])
+def home(name=None):
+	if request.method == 'POST':
+		return render_template('result.html', name=name)
+	else:
+		return "<p>Invalid Access Mr. Mathias</p>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
