27e6aa82fe3d63cc0b4565a2076e2bd6e84da96f
Duong Nguyen <davidnguyenT@gmail.com>
2014-02-26 09:28:33 -0600

    Added iOS app connectivity to server Requires the server to return JSON, not fully functional with self-signed certs

--- _Note iOS App/_Note/AppDelegate.m
+++ _Note iOS App/_Note/AppDelegate.m
@@ -2,8 +2,8 @@
 //  AppDelegate.m
 //  _Note
 //
-//  Created by Lyndon Quigley on 2/4/2014.
-//  Copyright (c) 2014 Lyndon Quigley. All rights reserved.
+//  COMP 4350 - Software Development 2
+//  Group 1: _Note
 //
 
 #import "AppDelegate.h"
@@ -14,9 +14,26 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    // Request server for JSON, url of server to request from
+    NSString *urlString = @"https://google.ca";
+    NSURL *url = [NSURL URLWithString:urlString];
+    
+    // HTTP request to server
+    NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:url];
+    [urlRequest setTimeoutInterval:10];
+    [urlRequest setHTTPMethod:@"GET"];
+    
+    //allocate a new operation queue
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    
+    // Establish connection
+    [NSURLConnection sendAsynchronousRequest:urlRequest queue:queue
+                           completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {} ];
+    
+    
     // Override point for customization after application launch.
     self.notes = [NSMutableArray arrayWithArray: @[
-            [Note noteWithText: @"I'm testing out this thing that I'm doing. Yay"]]];
+            [Note noteWithText: @"Will load the list of notes in this area here."]]];
     UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
     UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
     splitViewController.delegate = (id)navigationController.topViewController;
